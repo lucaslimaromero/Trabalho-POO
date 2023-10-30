3f402d91933c3f3ba3e2215948a10e9547f834d7
Lucas Lima Romero <lucasromero1702@gmail.com>
2023-10-30 20:32:09 -0300

    Simplificação esfera

--- PrototipoProjetoPOO/src/Modelo/Esfera.java
+++ PrototipoProjetoPOO/src/Modelo/Esfera.java
@@ -28,51 +28,19 @@ public class Esfera extends Personagem implements Serializable{
         super.autoDesenho();
         if(this.sentido == 'u'){
             if(!this.moveUp()) {
-                if (this.fase == 1) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 1);
-                } else if (this.fase == 2) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 2);
-                } else if (this.fase == 3) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 3);
-                } else if (this.fase == 4) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 4);
-                }
+                Desenho.acessoATelaDoJogo().addPersonagem(this, this.fase);
             }
         } else if(this.sentido == 'd'){
             if(!this.moveDown()) {
-                if (this.fase == 1) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 1);
-                } else if (this.fase == 2) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 2);
-                } else if (this.fase == 3) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 3);
-                } else if (this.fase == 4) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 4);
-                }
+                Desenho.acessoATelaDoJogo().addPersonagem(this, this.fase);
             }
         } else if(this.sentido == 'r'){
             if(!this.moveRight()) {
-                if (this.fase == 1) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 1);
-                } else if (this.fase == 2) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 2);
-                } else if (this.fase == 3) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 3);
-                } else if (this.fase == 4) {
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 4);
-                }
+                Desenho.acessoATelaDoJogo().addPersonagem(this, this.fase);
             }
         } else if(this.sentido == 'l'){
             if(!this.moveLeft()) {
-                if(this.fase == 1){
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 1);
-                } else if(this.fase == 2){
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 2);
-                } else if(this.fase == 3){
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 3);
-                } else if(this.fase == 4){
-                    Desenho.acessoATelaDoJogo().addPersonagem(this, 4);
-                }
+                Desenho.acessoATelaDoJogo().addPersonagem(this, this.fase);
             }
         }
     }
